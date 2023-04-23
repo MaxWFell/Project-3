f0b6d53005f0658ad9d62effd7f14d3dadc5286c
Max Fell <Maxwilliamfell@gmail.com>
2023-04-23 09:30:35 -0400

    fixed deployment need roses changes

--- .next/server/pages/_document.js
+++ .next/server/pages/_document.js
@@ -27,7 +27,7 @@ function Document() {
                     rel: "stylesheet",
                     href: "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css",
                     integrity: "sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N",
-                    crossorigin: "anonymous"
+                    crossOrigin: "anonymous"
                 })
             }),
             /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("body", {
@@ -37,13 +37,13 @@ function Document() {
                     /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("script", {
                         src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js",
                         integrity: "sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==",
-                        crossorigin: "anonymous",
-                        referrerpolicy: "no-referrer"
+                        crossOrigin: "anonymous",
+                        referrerPolicy: "no-referrer"
                     }),
                     /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("script", {
                         src: "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js",
                         integrity: "sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct",
-                        crossorigin: "anonymous"
+                        crossOrigin: "anonymous"
                     })
                 ]
             })
