d73e63dd3150de5df227c0f5075108c947cf681f
bshahata <117234613+bshahata@users.noreply.github.com>
2023-04-27 20:43:15 -0400

    updated looks

--- components/TeamMate.js
+++ components/TeamMate.js
@@ -1,7 +1,7 @@
 function TeamMate({ name, role, imagePath }) {
     return (
         <>
-            <img className="img-fluid rounded-circle shadow" src={imagePath} alt={name + `'s avator`} />
+            {/* <img className="img-fluid rounded-circle shadow" src={imagePath} alt={name + `'s avator`} /> */}
             <h5 className="mt-3 font-weight-bold">{name}</h5>
             <p className="lead">{role}</p>
         </>
--- pages/services.js
+++ pages/services.js
@@ -26,29 +26,29 @@ export async function getStaticProps() {
     const services = [
         {
             id: 1,
-            title: "Dog walking",
-            description: "We offer professional dog walking services for busy pet owners who want to ensure their furry friends get the exercise they need. Our experienced and reliable dog walkers will take your pup on regular walks around your neighborhood or local park, so you can have peace of mind knowing your dog is getting the attention and exercise they need."
+            title: "Freshly Packaged with Care",
+            description: "All orders are ready made fresh and tightly sealed, so you don't have to worry about any messes and most importantly contamination. "
         },
         {
             id: 2,
-            title: "Pet sitting",
-            description: "If you're planning a vacation or have to be away from home for an extended period, our pet sitting service can provide your furry friends with the care and attention they deserve. Our pet sitters are experienced and compassionate caregivers who will make sure your pets are fed, played with, and well-cared for while you're away."
+            title: "Super Fast Delivery",
+            description: "Whether it maybe a date night, family gathering, or just you, you're in safe hands knowing we offer guaranteed next day delivery."
         },
         {
             id: 3,
-            title: "Grooming",
-            description: "At our pet care center, we understand how important grooming is to the health and happiness of your pets. That's why we offer professional grooming services to keep your pets looking and feeling their best. From haircuts and baths to nail trims and ear cleaning, we'll make sure your pets are clean and comfortable."
+            title: "Options upon Options",
+            description: "We offer a wide variety of cuisines for all types of Foodies. The only limitation is your cravings."
         },
-        {
-            id: 4,
-            title: "Boarding",
-            description: "When you need to leave town, our pet boarding service provides a safe and comfortable environment for your pets to stay. We offer spacious accommodations, plenty of playtime, and personalized attention to ensure your pets are happy and healthy while you're away."
-        },
-        {
-            id: 5,
-            title: "Training",
-            description: " If you're looking to teach your pet new skills or overcome behavioral issues, our professional training services can help. We offer a variety of training programs for dogs of all ages and breeds, including obedience training, potty training, and specialized training for therapy animals or service dogs."
-        }
+        // {
+        //     id: 4,
+        //     title: "Boarding",
+        //     description: "When you need to leave town, our pet boarding service provides a safe and comfortable environment for your pets to stay. We offer spacious accommodations, plenty of playtime, and personalized attention to ensure your pets are happy and healthy while you're away."
+        // },
+        // {
+        //     id: 5,
+        //     title: "Training",
+        //     description: " If you're looking to teach your pet new skills or overcome behavioral issues, our professional training services can help. We offer a variety of training programs for dogs of all ages and breeds, including obedience training, potty training, and specialized training for therapy animals or service dogs."
+        // }
     ];
 
     return {
